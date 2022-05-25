222b21c66b2cb93a884bff69fcfc89719a051734
nhungbi <nhungvan16@gmail.com>
2022-05-25 13:30:22 -0500

    fn python unit test

--- python/character_match_spec.py
+++ python/character_match_spec.py
@@ -1,13 +1,26 @@
 from character_match import is_character_match
 
 # This should return a bunch of trues
-print(is_character_match('charm', 'march') == True)
-print(is_character_match('zach', 'attack') == False)
-print(is_character_match('CharM', 'mARcH') == True)
-print(is_character_match('abcde2', 'c2abed') == True)
+# print(is_character_match('charm', 'march') == True)
+# print(is_character_match('zach', 'attack') == False)
+# print(is_character_match('CharM', 'mARcH') == True)
+# print(is_character_match('abcde2', 'c2abed') == True)
 
-print("This test is for the challenge quesiton")
-print(is_character_match('Anna Madrigal', 'A man and a girl') == True)
+# print("This test is for the challenge quesiton")
+# print(is_character_match('Anna Madrigal', 'A man and a girl') == True)
 
 
 # Can you translate this driver code to unit tests?
+import unittest
+
+class TestCharMatch(unittest.TestCase):
+
+    def test_char_match(self):
+        self.assertEqual(is_character_match('charm', 'march'), True)
+        self.assertEqual(is_character_match('zach', 'attack'), False)
+        self.assertEqual(is_character_match('CharM', 'mARcH'), True)
+        self.assertEqual(is_character_match('abcde2', 'c2abed'), True)
+        self.assertEqual(is_character_match('Anna Madrigal', 'A man and a girl'), True)       
+
+if __name__ == '__main__':
+    unittest.main()
