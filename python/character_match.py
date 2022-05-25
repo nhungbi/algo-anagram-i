8267a1a9618dbac561194035171a0b97e3e1a115
nhungbi <nhungvan16@gmail.com>
2022-05-25 11:19:46 -0500

    i

--- python/character_match.py
+++ python/character_match.py
@@ -1,5 +1,41 @@
 # Don't forget to run your tests!
+from operator import is_
+
+def count_char(input_string):
+	"""
+	Given a string, return a dictonary with its character as keys
+	and values as the number of times the character shows up in the string.
+	"""
+	string_ = [char.lower() for char in input_string if char != ' '] #to account for uppercase
+	char_dict = {}
+	for char in string_:
+		if char in char_dict:
+			char_dict[char] +=1
+		else:
+			char_dict[char] = 1 #if not in dict, then add an entry with value 1
+
+	return char_dict
+
 
 def is_character_match(string1, string2):
-	pass
-	# Your code here
+	"""
+	Given two strings, return True if the number characters match.
+	"""
+	
+	char_dict1 = count_char(string1)
+	char_dict2 = count_char(string2)
+
+	for k,v in char_dict1.items():
+		if k not in char_dict2: #a char in string1 not in string2
+			return False
+		elif k in char_dict2:
+			if char_dict1[k] != char_dict2[k]:
+				return False
+		
+	return True
+
+
+
+
+	
+	
